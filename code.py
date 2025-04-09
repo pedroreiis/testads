c67ec967c7186b80d07796797a2ce8027953fd12
João Victor Viana Oliveira Silva (gaspar) <joaovctrjvv90@gmail.com>
2025-04-08 21:23:33 -0300

    potencia atualizada

--- code.py
+++ code.py
@@ -3,3 +3,8 @@ print('ESTA É UMA CALCULADORA')
 num1 = int(input('digite o primeiro numero: '))
 num2 = int(input(' digite o segundo numero: '))
 operador = input('escolha o operador: ')
+
+n1= int(input("digite o primeiro numero para realizar a potenciação"))
+n2= int(input("digite o segundo numero para realizar a potenciação"))
+resul=n1**n2
+print(result)
