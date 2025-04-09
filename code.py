87476e741e8ef2d487f7b11e97f7082e36a6c1cf
Aughusto Jose <ed.fsf@outlook.com>
2025-04-08 21:28:16 -0300

    Ajuste no codigo para mostrar uma mulitplicaçao

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
 print('ESTA É UMA CALCULADORA')
 num1 = int(input("Coloque o primeiro número: "))
 num2 = int(input("Coloque o segundo número: "))
-print(num1, "*", num2, "=", num1 * num2)
+if operador == "*":
+    print(f"{num1} * {num2} = {num1 * num2}")
+
