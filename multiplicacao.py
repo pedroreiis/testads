9cd062b8e4d951ec8b7097155f3cd82719655e72
João Victor Viana Oliveira Silva (gaspar) <joaovctrjvv90@gmail.com>
2025-04-08 21:06:00 -0300

    branch potencia

---
+++ multiplicacao.py
@@ -0,0 +1,4 @@
+n1 = int(input("digite o primeiro numero para realizar a multiplicação"))
+n2 = int(input("digite o segundo numero para realizar a multiplicação"))
+result=n1*n2
+print(result)
---
+++ potencia.py
@@ -0,0 +1,4 @@
+n1= int(input("digite o primeiro numero para realizar a potenciação"))
+n2= int(input("digite o segundo numero para realizar a potenciação"))
+resul=n1**n2
+print(result)
