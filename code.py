65c92f79ce3d2fe6cc45cc1b9a91577177664789
Jotinha <joao.jobibo@gmail.com>
2025-04-15 20:31:25 -0300

    meu nome é jk

--- code.py
+++ code.py
@@ -2,4 +2,5 @@ p[unix] (20:59 31/12/1969)                                      0,0-1 All
 print('ESTA É UMA CALCULADORA')
 num1 = int(input('digite o primeiro numero: '))
 num2 = int(input(' digite o segundo numero: '))
-operador = input('escolha o operador: ')
+operador = input('escolha o operador:')
+iii
