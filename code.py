7f29b4a1143238ff850e669824125c9d1ef31484
João Victor Viana Oliveira Silva (gaspar) <joaovctrjvv90@gmail.com>
2025-04-15 19:55:38 -0300

    correto

--- code.py
+++ code.py
@@ -1,10 +1,6 @@
 p[unix] (20:59 31/12/1969)                                      0,0-1 All
 print('ESTA É UMA CALCULADORA')
-num1 = int(input('digite o primeiro numero: '))
-num2 = int(input(' digite o segundo numero: '))
-operador = input('escolha o operador: ')
-
 n1= int(input("digite o primeiro numero para realizar a potenciação"))
 n2= int(input("digite o segundo numero para realizar a potenciação"))
 resul=n1**n2
-print(result)
+print(resul)
