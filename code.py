743146c080a28a868559fe6f0afccc081d524b78
pedroReis <reisalcantaraph@gmail.com>
2025-04-15 20:23:19 -0300

    Fazendo um codigo para mostrar uma soma

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 p[unix] (20:59 31/12/1969)                                      0,0-1 All
 print('ESTA É UMA CALCULADORA')
-num1 = int(input('digite o primeiro numero: '))
-num2 = int(input(' digite o segundo numero: '))
-operador = input('escolha o operador: ')
+if operador == "+":
+n1= float(input("Digite um número"))
+n2= float(input("Digite um número"))
+print(f"Sua soma é igual a {n1 + n2}")
