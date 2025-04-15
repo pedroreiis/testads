84097dc46cc53cce026727161762283085dcc1c1
Leonardo Coelho <leonardocoelhonunes@gmail.com>
2025-04-15 20:06:04 -0300

    estou fazendo um codigo de subtração

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 p[unix] (20:59 31/12/1969)                                      0,0-1 All
 print('ESTA É UMA CALCULADORA')
-num1 = int(input('digite o primeiro numero: '))
-num2 = int(input(' digite o segundo numero: '))
-operador = input('escolha o operador: ')
+num1 = int(input('digite o primeiro numero inteiro'))
+num2 = int(input('digite o segundo numero inteiro: '))
+subtração = num1 - num2
+print(f"a subtração entre o numero {num1} e {num2} é {subtração})
+
